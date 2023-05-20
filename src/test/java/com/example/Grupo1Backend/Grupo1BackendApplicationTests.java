3806c165ff3bfe8c7e4c5e7c6410447dcfe0a3e2
Andr3s593 <rac.piedra@yavirac.edu.ec>
2023-05-20 17:51:54 -0500

    Inicio del Proyecto Grupo 1

---
+++ src/test/java/com/example/Grupo1Backend/Grupo1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grupo1Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
