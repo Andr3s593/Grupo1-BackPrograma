a74c2d503fec5ff47c675a71bfe825d0c080cca1
Andr3s593 <rac.piedra@yavirac.edu.ec>
2023-05-20 18:17:02 -0500

    Crud Actividad realizado por Ricardo

---
+++ src/main/java/com/example/Grupo1Backend/Actividad/Actividad.java
@@ -0,0 +1,19 @@
+package com.example.Grupo1Backend.Actividad;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Actividad {
+    // Atributos: Delimitador de Acceso, Tipo de Dato, Nombre del Atributo
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nombre;
+    private String fechainicio;
+    private String fechafin;
+}
